656963d52a1284a6a3a4863a1d7b5ceca848eeba
AlexDemayo <alexd_91@hotmail.com>
2020-06-19 00:26:49 -0300

    la home

--- 02_sprint/src/controllers/homeController.js
+++ 02_sprint/src/controllers/homeController.js
@@ -1,6 +1,12 @@
+const jsonModel = require('../models/jsonModel');
+const bookModel = jsonModel('booksData');
 
 const homeController = {
     
+        root : function(req,res){
+            const books = bookModel.leerJson();
+            res.render('index', {books})
+        }  
 }
 
 
