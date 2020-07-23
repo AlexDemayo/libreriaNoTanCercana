32211e77fbd344ce9950be75642410150ad9e0fc
nahuelcrisa <nahuel.crisafio@hotmail.com>
2020-07-23 06:43:11 -0300

    User profile y Style varios

--- 02_sprint/src/controllers/usersController.js
+++ 02_sprint/src/controllers/usersController.js
@@ -1,65 +1,65 @@
 const jsonModel = require('../models/jsonModel');
 const userModel = jsonModel('users');
 const bcrypt = require('bcryptjs');
-const {validationResult} = require('express-validator')
+const { validationResult } = require('express-validator');
 
 const usersController = {
-    logYreg: function(req,res){
-        res.render('login-register')
-    }, 
+	logYreg: function(req, res) {
+		res.render('login-register');
+	},
 
-    register: function(req,res){
-        const errors = validationResult(req);
+	user: function(req, res) {
+		res.render('user');
+	},
 
-        if(errors.isEmpty()){
-            delete req.body.confirmPasswordReg;
-            req.body.passwordReg = bcrypt.hashSync(req.body.passwordReg, 10);
+	register: function(req, res) {
+		const errors = validationResult(req);
 
-            userModel.create({
-                id : " ",
-                ...req.body
-            })
-    
-            return res.redirect('/');
+		if (errors.isEmpty()) {
+			delete req.body.confirmPasswordReg;
+			req.body.passwordReg = bcrypt.hashSync(req.body.passwordReg, 10);
 
-        }else{
-            return res.render('login-register', {errorsRegister : errors.mapped(), oldRegister : req.body});
-        }
-    },
+			userModel.create({
+				id: ' ',
+				...req.body
+			});
 
-    login: function(req,res){
-        const errors = validationResult(req);
+			return res.redirect('/');
+		} else {
+			return res.render('login-register', { errorsRegister: errors.mapped(), oldRegister: req.body });
+		}
+	},
 
-        if(errors.isEmpty()){
-            let user = userModel.findBySomething(user => user.emailReg == req.body.emailLog);
-            
-            delete user.passwordReg;
+	login: function(req, res) {
+		const errors = validationResult(req);
 
-            req.session.user = user; //lo guarda en sesión
+		if (errors.isEmpty()) {
+			let user = userModel.findBySomething((user) => user.emailReg == req.body.emailLog);
 
-            if(req.body.remember){
-                res.cookie('remember', user.emailReg, { maxAge: 1000 * 60 * 60 * 24 }); 
-            } 
-            // si el usuario puso remember, guardamos el mail por un día 
+			delete user.passwordReg;
 
-            return res.redirect('/');
-            
-        } else {
-            return res.render('login-register', {errorsLogin : errors.mapped(), oldLogin : req.body});
-        }
-    },
-    
-    logout: function(req, res) {
-    
-        req.session.destroy();
-    
-        if(req.cookies.email){
-          res.clearCookie('remember');
-        }
-    
-        return res.redirect('/')
-    
-      }
+			req.session.user = user; //lo guarda en sesión
+
+			if (req.body.remember) {
+				res.cookie('remember', user.emailReg, { maxAge: 1000 * 60 * 60 * 24 });
+			}
+			// si el usuario puso remember, guardamos el mail por un día
+
+			return res.redirect('/');
+		} else {
+			return res.render('login-register', { errorsLogin: errors.mapped(), oldLogin: req.body });
+		}
+	},
+
+	logout: function(req, res) {
+		req.session.destroy();
+
+		if (req.cookies.email) {
+			res.clearCookie('remember');
+		}
+
+		return res.redirect('/');
+	}
 };
 
-module.exports = usersController
+module.exports = usersController;
--- 02_sprint/src/routes/users.js
+++ 02_sprint/src/routes/users.js
@@ -2,13 +2,12 @@ const express = require('express');
 const router = express.Router();
 const usersController = require('../controllers/usersController');
 const validator = require('../middleWares/validator');
-const {body} = require('express-validator');
-
-
+const { body } = require('express-validator');
 
 router.get('/login-register', usersController.logYreg);
+router.get('/user', usersController.user);
 router.post('/register', validator.register, usersController.register);
 router.post('/login', validator.login, usersController.login);
-router.post('/logout', usersController.logout)
+router.post('/logout', usersController.logout);
 
 module.exports = router;
