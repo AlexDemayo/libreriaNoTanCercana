cbe9be7d6d1618984cea738fd7fec3f826d2e397
camila-gil <gilcamilat@gmail.com>
2020-07-23 22:40:23 -0300

    commit

--- 02_sprint/src/routes/books.js
+++ 02_sprint/src/routes/books.js
@@ -1,13 +1,25 @@
 const express = require('express');
 const router = express.Router();
 const booksController = require('../controllers/booksController');
+const multer = require('multer');
+
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, '../../public/images')
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.fieldname + '-' + Date.now())
+    }
+  })
+   
+  var upload = multer({ storage: storage })
 
 /* GET users listing. */
 
-router.get('/create',booksController.createBook);
+router.get('/create', booksController.createBook);
 
 router.get('/:id', booksController.detail);
 
-// router.post('/store', boo)
+
 
 module.exports = router;
