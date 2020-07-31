04e0b0fa2863fb536c1bc7e253096ab28ece992b
AlexDemayo <alexd_91@hotmail.com>
2020-07-31 20:37:57 -0300

    toggle2

--- 02_sprint/public/js/profile-menu.js
+++ 02_sprint/public/js/profile-menu.js
@@ -1,16 +1,16 @@
 window.addEventListener('load', function() {
 	let configButton = document.querySelector('.js-a');
 	let userform = document.querySelector('.user-info');
-	let x = document.querySelector('.user-info-completo');
-	x.style.display = "none";
+	let userCompleto = document.querySelector('.user-info-completo');
+	
 	configButton.addEventListener('click', function() {
 		
 		
-			if (x.style.display === "none") {
-			  x.style.display = "block";
+			if (userCompleto.style.display === "none") {
+				userCompleto.style.display = "block";
 			  userform.style.display = "none"
 			} else {
-			  x.style.display = "none";
+				userCompleto.style.display = "none";
 			  userform.style.display = "block"
 			}
 		
