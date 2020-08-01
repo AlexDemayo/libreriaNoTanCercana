8a4db12e169d0028b42181fc7ae55984a61aaf57
nahuelcrisa <nahuel.crisafio@hotmail.com>
2020-08-01 00:12:42 -0300

    fix profile user

--- 02_sprint/public/js/profile-menu.js
+++ 02_sprint/public/js/profile-menu.js
@@ -1,20 +1,18 @@
 window.addEventListener('load', function() {
 	let configButton = document.querySelector('.js-a');
 	let userform = document.querySelector('.user-info');
-	let userCompleto = document.querySelector('.user-info-completo');
-	
+	let userCompleto = document.querySelector('.user-info-config');
+
 	configButton.addEventListener('click', function() {
-		
-		
-			if (userCompleto.style.display === "none") {
-				userCompleto.style.display = "block";
-			  userform.style.display = "none"
-			} else {
-				userCompleto.style.display = "none";
-			  userform.style.display = "block"
-			}
-		
-			
-		
-		});
+		if (userCompleto.style.display === 'none') {
+			userCompleto.style.display = 'block';
+			userform.style.display = 'none';
+		} else {
+			userCompleto.style.display = 'none';
+			userform.style.display = 'block';
+		}
+	});
+
+	let aprofile = document.querySelector('.a-profile');
+	aprofile.classList.add('marginpf');
 });
