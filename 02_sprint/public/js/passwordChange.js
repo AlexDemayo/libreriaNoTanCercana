6fee0d7e1a1061b403b77e7a3a5eb95a26cdebf9
nahuelcrisa <nahuel.crisafio@hotmail.com>
2020-08-26 03:36:10 -0300

    commit

---
+++ 02_sprint/public/js/passwordChange.js
@@ -0,0 +1,21 @@
+window.addEventListener('load', function() {
+	let passwordButton = document.querySelector('.js-b');
+	let userform = document.querySelector('.user-info');
+    let passwordForm = document.querySelector(".user-password-config");
+
+	passwordButton.addEventListener('click', function() {
+		if (passwordForm.style.display === 'none') {
+			passwordForm.style.display = 'block';
+			userform.style.display = 'none';
+		} else {
+			passwordForm.style.display = 'none';
+			userform.style.display = 'block';
+		}
+    });
+    
+    let aprofile = document.querySelector('.a-profile');
+	aprofile.classList.add('marginpf');
+
+
+
+});
