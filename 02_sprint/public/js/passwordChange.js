afccafdc8b2c0f3ca929c84c86f8ab7125b95d56
nahuelcrisa <nahuel.crisafio@hotmail.com>
2020-08-28 03:49:50 -0300

    fix de passwordChange

--- 02_sprint/public/js/passwordChange.js
+++ 02_sprint/public/js/passwordChange.js
@@ -2,14 +2,17 @@ window.addEventListener('load', function() {
 	let passwordButton = document.querySelector('.js-b');
 	let userform = document.querySelector('.user-info');
     let passwordForm = document.querySelector(".user-password-config");
+    let userCompleto = document.querySelector('.user-info-config');
 
 	passwordButton.addEventListener('click', function() {
 		if (passwordForm.style.display === 'none') {
 			passwordForm.style.display = 'block';
-			userform.style.display = 'none';
+            userform.style.display = 'none';
+            userCompleto.style.display = 'none';
 		} else {
 			passwordForm.style.display = 'none';
-			userform.style.display = 'block';
+            userform.style.display = 'block';
+            userCompleto.style.display = 'none';
 		}
     });
     
